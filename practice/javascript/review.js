2a1a209d2c4261b5b8b266abd22d9527c4ac34af
Beau Witka <bwitka@gmail.com>
2019-10-21 10:24:51 -0700

    Initial commit: practice reviewing basic JavaScript

---
+++ practice/javascript/review.js
@@ -0,0 +1,58 @@
+// Storing information in JavaScript: variables
+let sampleVariable = 1000;
+
+/* =========================================================================== */
+
+// JavaScript data types: strings, numbers, booleans, null, undefined, symbol.
+
+// String:
+("Hello, World!");
+
+// Number:
+2910.30234;
+1912;
+
+// Boolean:
+true;
+false;
+
+// Null:
+null;
+
+// Undefined:
+undefined;
+
+// Symbol:
+/* 
+A value having the data type Symbol can be referred to as a "Symbol value".  In a JavaScript run-time environment, a symbol value is created by invoking the function Symbol, which dynamically produces an anonymous, unique value.
+*/
+let Sym1 = Symbol("Sym");
+
+/* =========================================================================== */
+
+// Operators: + - * / %
+// note: the + operator is also uses in concatenation.
+
+/* =========================================================================== */
+
+// Comparison operators:
+/* 
+== is equal to?
+!= is not equal to
+*/
+
+/* =========================================================================== */
+
+// Relational operators:
+/*
+> greater than
+>= greater than or equal to
+< less than
+<= less than or equal to
+*/
+
+/* =========================================================================== */
+
+// Falsy values: false, 0, "", null, NaN, undefined
+
+// Truthy values:
