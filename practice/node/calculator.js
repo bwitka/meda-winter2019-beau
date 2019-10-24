b7deec79f7519853128497e6477cb42f5e91ac9d
Beau Witka <bwitka@gmail.com>
2019-10-24 11:56:47 -0700

    Modify calculator.js code so that the user enters a valid mathematical operator in between the two numbers

--- practice/node/calculator.js
+++ practice/node/calculator.js
@@ -1,16 +1,33 @@
+// Note: Try adding a prompt for the user in the future. One node module you can use is Inquirer.js (https://alligator.io/nodejs/interactive-command-line-prompts/).
+
 // Renaming ARGV array for easier reading.
 // When using ARGV, start with index 2 - because indexes 0 and 1 are not useful in our code.
 let terminalArguments = process.argv;
 
+let mathOperation = terminalArguments[3];
 let arg1 = parseInt(terminalArguments[2]);
-let arg2 = parseInt(terminalArguments[3]);
+let arg2 = parseInt(terminalArguments[4]);
 
 if (isNaN(arg1) || isNaN(arg2)) {
   console.log(
-    "You are missing one or two numbers, please type in two numbers after the command with spaces. Example: node calculator.js 50 100"
+    "You are missing one or two numbers or a legitimate mathematical operator. Please enter two numbers separated by a mathematical operator after the command. Be sure to include a space between all three values. Example: node calculator.js 50 plus 100"
   );
 
   return;
+}
+
+if (mathOperation == "plus") {
+  add(arg1, arg2);
+} else if (mathOperation == "minus") {
+  subtract(arg1, arg2);
+} else if (mathOperation == "times") {
+  multiply(arg1, arg2);
+} else if (mathOperation == "divided-by") {
+  divide(arg1, arg2);
+} else if (mathOperation == "modulus") {
+  getRemainder(arg1, arg2);
+} else {
+  console.log("Sorry we did not understand the operation you requested.");
 }
 
 function add(a, b) {
@@ -38,8 +55,8 @@ function getRemainder(a, b) {
   console.log(`The remainder of ${a} and ${b} is ${remainder}.`);
 }
 
-add(arg1, arg2);
-subtract(arg1, arg2);
-multiply(arg1, arg2);
-divide(arg1, arg2);
-getRemainder(arg1, arg2);
+// add(arg1, arg2);
+// subtract(arg1, arg2);
+// multiply(arg1, arg2);
+// divide(arg1, arg2);
+// getRemainder(arg1, arg2);
