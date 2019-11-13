032a32a936b4c7ea56d7bf9c212b2baac801f318
Beau Witka <bwitka@gmail.com>
2019-11-13 12:51:36 -0800

    Copy and paste Eduardo's code into requests directory due to errors in my own code

--- practice/express/requests/index.js
+++ practice/express/requests/index.js
@@ -1,78 +1,94 @@
-// include the fs package (built in)
+// Include into our code, the FS package (builtin)
 const fs = require("fs");
-// include express.js, via npm
+// Includes into our code, the Express.js, provided by NPM.
 const express = require("express");
-// include bodyParser, which comes w/ express.js
+// Includes into our code Body Parser, comes with Express.js
 const bodyParser = require("body-parser");
-// create an express.js server object
+// Create an Express.js server object.
 const app = express();
-// include the HTTP package (built in) and attach the express server object to the HTTP object
+// Includes the HTTP package (builtin), and we attach our Express.js server object to the HTTP object.
 const http = require("http").Server(app);
-// create a variable that will hold the port number we want to use
+// We create a variable that will hold the port number we want to use.
 const port = 3000;
-// pass the port variable to the listen function for the HTTP server
+// We pass the port variable to the listen function for the HTTP server.
 http.listen(port);
-// let the developer know that express.js is now running
-console.log(`Express server is running on port number ${port}.`);
 
-// Need to read data sent through POST request:
+// Signifying the Developer that Express.js is now running.
+console.log("Express server running on port " + port);
+
+// Needed to read data sent through POST request.
 app.use(bodyParser.json());
-// give bodyParser specific options to run off of
+// Gives Body Parser specific options to run off of.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/******** BELOW = CUSTOM CODE FOR EXPRESS.JS  ********/
+// Custom Code for Express.js after this line.
 
-// Routes:
-// first argument is the route path; second argument is the directory path to load when someone requests the route name
+// Variable to make sure the file name is the same every time.
+let filename = "commentHistory.json";
+
+// Routes
+// First Argument is the route name, second argument is directory to load when someone requests the route name.
 app.use("/", express.static("client/"));
 
-// next line responds to any post request (two arguments: route, arrow function that will fire whenever it gets a request):
-// a http post handler called /submitComment
-// first argument = HTTP post route name, second argument = callback function. The callback function will run every time someone requests the /submitComment post handler
-// callback function parameters: request holds the object that was sent to us through the Internet; the response object holds a reference of where to sent the response to (e.g. IP address)
+// An HTTP Post Handler called /submitComment.
+// First argument is HTTP Post route name, second argument is the callback function. The callback function will run every time someone requests the /submitComment Post handler.
+// Callback function parameters: request holds the object that was sent to us through the Internet, the response object holds a reference of where to send the response to (like IP address).
 app.post("/submitComment", (request, response) => {
-  // rename the data sent to us (located in request.body) to something simpler like objectFromRequest
+  // Rename the data sent to us (located in request.body) to something simpler like objectFromRequest.
   let objectFromRequest = request.body;
 
   console.log(objectFromRequest);
 
-  // variable to make sure the file name is the sane every time
-  let filename = "commentHistory.json";
+  // let text = objectFromRequest.message;
+  // console.log("We recieved a request for/submitComment: " + text);
 
-  // if the file exists...
+  // If the file exists do...
   if (fs.existsSync(filename)) {
-    // ...read the file and store the contents in the variable comments
+    // ...read the file and store the contents in the variable comments...
     let comments = fs.readFileSync(filename, "utf8");
-    // ...parse the JSON and reuse the same comments variable
+    // ...parse the JSON and reuse the same comments variable...
     comments = JSON.parse(comments);
-    // ...add the new objectFromRequest object to the array inside of the comments object
+    // ...add the new objectFromRequest object to the array inside of the comments object...
     comments.commentsArray.push(objectFromRequest);
-    // ...then convert comments back into a string as JSON
+    // ...then convert comments back into a string as JSON...
     comments = JSON.stringify(comments);
-    // ...finally save the JSON string to a file
-    fs.writeFileSync(filename, comments, "UTF8");
-
-    console.log("New comment saved to hard drive.");
+    // ...finally save the JSON string to a file.
+    fs.writeFileSync(filename, comments, "utf8");
+    console.log("New Comment Saved to Hard Drive!");
   } else {
-    // if the file doesn't exist...
-    // ...we pre-build the object (which includes the comment we just received) to save as JSON
-    let comments = { commentsArray: [objectFromRequest] };
-    // ...convert it to an actual JSON string
+    // If the file doesn't exists...
+    // ...We prebuild the object (which includes the comment we just recieve) to save as JSON...
+    let comments = {
+      commentsArray: [objectFromRequest]
+    };
+    // ...Convert it actual JSON String...
     comments = JSON.stringify(comments);
-    // ...finally save the JSON string to a new file
-    fs.writeFileSync(filename, comments, "UTF8");
-
+    // ...Finally save JSON string to new File.
+    fs.writeFileSync(filename, comments, "utf8");
     console.log(
-      "Note: No existing file detected. Creating new file and saving comment to hard drive."
+      "Note: No Save File Detected, creating New File. New Comment Saved to Hard Drive!"
     );
   }
 
   // {commentsArray: []}
 
-  // if you don't want to send any data back, you can use .sendStatus(). You can only use sendStatus() or send once to 'fulfill' front-end request.
+  // If you don't want to send any data back, you can use .sendStatus(). You can only use sendStatus or send once to "fulfill" front-end request.
 
   response.sendStatus(200);
 });
 
-// create another post handler:
-app.post("/loadComments", (request, response) => {});
+// A second HTTP Post Handler called /loadComments
+app.post("/loadComments", (request, response) => {
+  // Check if the JSON file exists...
+  if (fs.existsSync(filename)) {
+    // ...If it exists then read it...
+    let comments = fs.readFileSync(filename, "utf8");
+    // ...and convert to a JavaScript Object...
+    comments = JSON.parse(comments);
+    // ...finally send it to the requester.
+    response.send(comments);
+  } else {
+    // ...If it doesn't exist, then send an error 500 to the requester.
+    response.sendStatus(500);
+  }
+});
