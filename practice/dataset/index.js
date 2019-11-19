b21d6d41a09214cccca7b8ee046fdb7557ee9e33
Beau Witka <bwitka@gmail.com>
2019-11-19 09:35:02 -0800

    Add comments to index.js (dataset project)

--- practice/dataset/index.js
+++ practice/dataset/index.js
@@ -7,40 +7,53 @@ const dataset = fs.readFileSync("Eviction_Notices.csv", "utf8");
 
 // Converted string into array where each new line is a new index.
 const dataArray = dataset.split("\n");
+// Removing the last index because it is empty.
+dataArray.pop();
+// Removing first line, but keeping it because we will use it in the future.
+let headings = dataArray.shift();
 
-console.log(dataArray.length);
-
+// Counter for the amount of 2018 entries we found.
 let total = 0;
 
+// Loop through all the entries of the dataset (not including headings, and last empty line).
 for (let i = 0; i < dataArray.length - 1; i++) {
+  // Test the entry using the is2018 function.
   let pass = is2018(dataArray[i]);
 
-  // "hello"
-  // ""
-
+  // If pass is true, then add a 1 to counter.
   if (pass === true) {
-    console.log("The line entry " + i + " is from the year 2018!");
+    //console.log("The line entry " + i + " is from the year 2018!");
     total++;
+    // If we encounter an issue, tell us about it.
   } else if (pass === 1) {
     console.log("The line " + (i + 1) + "has issues! please check.");
     console.log(dataArray[i]);
   }
+
+  // Otherwise if other than 2018, move on to the next entry.
 }
 
+// Finally console log the total amount of entries that matched 2018.
 console.log(total);
 
 // This accepts a string only, returns true, false, or 1 if issue.
 function is2018(entry) {
+  //Create an array that splits up the individual columns of the entry.
   const singleEntryArray = entry.split(",");
 
+  // Grab the date index, and split that by its day, month and year, store in another array.
   const entryDate = singleEntryArray[5].split("/");
 
+  // Using the array with the individual date parts, check the year index and to 2018.
   if ("2018" == entryDate[2]) {
+    // If it is 2018, return true.
     return true;
   } else if (undefined === entryDate[2]) {
+    // If there is no value in index 2, that means there was an issue.
     console.log("We found the issue with the line:\n" + entry);
     return 1;
   } else {
+    // If it is not 2018, return false.
     return false;
   }
 }
