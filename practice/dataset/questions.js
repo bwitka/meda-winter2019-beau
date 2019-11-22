0bc3deffede1135927b918b4c558a17fc76dcc6e
Beau Witka <bwitka@gmail.com>
2019-11-21 18:54:08 -0800

    Add code that answers questions 3 and 4 to questions.js file

--- practice/dataset/questions.js
+++ practice/dataset/questions.js
@@ -131,6 +131,7 @@ for (let i = 0; i < dataObjectsArray.length; i++) {
   ) {
     bothTotal++;
   }
+  // console.log(`The current object is: ${JSON.stringify(currentObject)}.`);
 }
 
 console.log(`
@@ -145,6 +146,42 @@ console.log(`
 
 // Question 3.)
 console.log("Which zip code had the most evictions for 2018?");
+
+/* Note: the following code was borrowed from...
+https://medium.com/@AmJustSam/how-to-find-most-frequent-item-of-an-array-12015df68c65
+...and modified for my needs. The comments are my own understanding of the code.
+*/
+
+// Create an empty object to store the occurrence of each zip code.
+let zipCollection = {};
+// Compare each zip code count to a base value of 0.
+let baseValue = 0;
+// Create a variable that will store value for most frequent zip code.
+let mostFrequentZip;
+
+// Loop over the data array and create a counter for each SF zip code.
+for (let i = 0; i < dataObjectsArray.length; i++) {
+  // Current zip code at time of iteration.
+  let currentZip = dataObjectsArray[i].zip;
+
+  // If current zip code is being passed over for first time, set its value to 1.
+  if (zipCollection[currentZip] === undefined) {
+    zipCollection[currentZip] = 1;
+    // If current zip has been passed over previously, increment its value by 1.
+  } else {
+    zipCollection[currentZip] += 1;
+  }
+
+  // If value of current zip code is larger than baseValue (initially set to 0), set baseValue equal to value of current zip, and set value of most frequent zip to be the same as current zip.
+  if (zipCollection[currentZip] > baseValue) {
+    baseValue = zipCollection[currentZip];
+    mostFrequentZip = currentZip;
+  }
+}
+
+console.log(
+  `The zip code with the highest number of evictions for 2018 is ${mostFrequentZip}.`
+);
 
 // Create an array of San Francisco zip codes:
 const zipCodes = [
@@ -199,6 +236,79 @@ const zipCodes = [
   94177,
   94188
 ];
+
+// Create an object that matches SF zip codes with their neighborhoods
+
+let sfNeighborhoodsByZip = {
+  94102: "Hayes Valley/Tenderloin/North of Market",
+  94103: "South of Market",
+  94104: "Financial District",
+  94105: "Financial District South",
+  94107: "Potrero Hill",
+  94108: "Chinatown",
+  94109: "Polk/Russian Hill (Nob Hill)",
+  94110: "Inner Mission/Bernal Heights",
+  94111: "Embarcadero",
+  94112: "Ingelside-Excelsior/Crocker-Amazon",
+  94114: "Castro/Noe Valley",
+  94115: "Western Addition/Japantown",
+  94116: "Parkside/Forest Hill",
+  94117: "Haight-Ashbury",
+  94118: "Inner Richmond",
+  94119: "Van Ness/Civic Center",
+  94120: "Van Ness/Civic Center",
+  94121: "Outer Richmond",
+  94122: "Sunset",
+  94123: "Marina",
+  94124: "Bayview-Hunters Point",
+  94125: "Van Ness/Civic Center",
+  94126: "Van Ness/Civic Center",
+  94127: "St. Francis Wood/Miraloma/West Portal",
+  94129: "Presidio",
+  94130: "Treasure Island",
+  94131: "Twin Peaks-Glen Park",
+  94132: "Lake Merced",
+  94133: "North Beach/Chinatown",
+  94134: "Visitacion Valley/Sunnydale",
+  94137: "Van Ness/Civic Center",
+  94139: "Van Ness/Civic Center",
+  94140: "Van Ness/Civic Center",
+  94141: "Van Ness/Civic Center",
+  94142: "Van Ness/Civic Center",
+  94143: "Van Ness/Civic Center",
+  94144: "Van Ness/Civic Center",
+  94145: "Van Ness/Civic Center",
+  94146: "Van Ness/Civic Center",
+  94147: "Van Ness/Civic Center",
+  94151: "Van Ness/Civic Center",
+  94158: "Mission Bay",
+  94159: "Van Ness/Civic Center",
+  94160: "Van Ness/Civic Center",
+  94161: "Van Ness/Civic Center",
+  94163: "Van Ness/Civic Center",
+  94164: "Van Ness/Civic Center",
+  94172: "Van Ness/Civic Center",
+  94177: "Van Ness/Civic Center",
+  94188: "Van Ness/Civic Center"
+};
+
+// <<< BONUS >>> Question 4.)
+console.log("Which neighborhood had the most evictions in the year 2018?");
+
+// Create an array of the keys from the sfNeighborhoodsByZip object. Loop through that array and compare each key with the mostFrequentZip variable. When they match, store the value for that key in the neighborhood variable.
+let zipCodeKeys = Object.keys(sfNeighborhoodsByZip);
+let neighborhood;
+for (let i = 0; i < zipCodeKeys.length; i++) {
+  if (zipCodeKeys[i] == mostFrequentZip) {
+    neighborhood = sfNeighborhoodsByZip[zipCodeKeys[i]];
+  }
+}
+
+console.log(
+  `The neighborhood with the most evictions in 2018 was ${neighborhood}.`
+);
+
+/******************************************************************************/
 
 function stringToObject(string) {
   // Split up the entry string into its individual parts.
