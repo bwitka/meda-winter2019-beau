aafffcd62a61f2cac632b7a0f97b73edcbc64f90
Beau Witka <bwitka@gmail.com>
2019-11-27 14:06:59 -0800

    Add all base code to index.js and questions.js in order to create 2018entries.json and JS object for data manipulation or query

---
+++ homework/soloDatasetAssignment/index.js
@@ -0,0 +1,67 @@
+// include node packages to use in project:
+const fs = require("fs");
+const lodash = require("lodash");
+
+// Read the dataset file with fs.readFileSync and store info in a variable.
+const dataset = fs.readFileSync("Film_Locations_in_San_Francisco.csv", "utf8");
+
+// Convert dataset variable into an array, where each line is a new index.
+const dataArray = dataset.split("\n");
+
+// Remove the last index from array because it is empty.
+dataArray.pop();
+
+// Create a variable that will store the first line/index of dataArray array.
+let headings = dataArray.shift();
+
+// Create object to store all data from SFData dataset that is from the year 2018.
+let json2018Data = {
+  headings: headings,
+  data: []
+};
+
+// Create a function to separate out all dataset entries that match the year 2018:
+function is2018(entry) {
+  //Create an array that splits up the individual columns of the entry.
+  const singleEntryArray = entry.split(",");
+
+  // Grab the date index, and split that by its day, month and year, store in another array.
+  const releaseYear = singleEntryArray[1];
+
+  // Using the array with the individual date parts, check the year index and to 2018.
+  if (releaseYear === "2018") {
+    // If it is 2018, return true.
+    return true;
+  } else if (releaseYear === undefined) {
+    // If there is no value in index 2, that means there was an issue.
+    console.log("We found the issue with the line:\n" + entry);
+    return 1;
+  } else {
+    // If it is not 2018, return false.
+    return false;
+  }
+}
+
+// Loop through all the entries of the dataset (not including headings or the last [empty] line) in order to push the data matching the year 2018 into our json2018Data object.
+for (let i = 0; i < dataArray.length - 1; i++) {
+  // Test the entry using the is2018 function.
+  let pass = is2018(dataArray[i]);
+
+  // If pass is true...
+  if (pass === true) {
+    // ...grab current line and add it to JSON object array.
+    json2018Data.data.push(dataArray[i]);
+    // If we encounter an issue, tell us about it.
+  } else if (pass === 1) {
+    console.log("The line " + (i + 1) + "has issues! please check.");
+    console.log(dataArray[i]);
+  }
+
+  // Otherwise if other than 2018, move on to the next entry.
+}
+
+// Convert json2018Data object to JSON string:
+let jsonString = JSON.stringify(json2018Data);
+
+// Write the JSON string to a file using fs.writeFileSync():
+fs.writeFileSync("2018entries.json", jsonString, "utf8");
---
+++ homework/soloDatasetAssignment/questions.js
@@ -0,0 +1,53 @@
+// include the fs package to read files:
+const fs = require("fs");
+
+// Build a class for the objects to be created from the Film_Locations data:
+class FilmLocations {
+  constructor(title, releaseYear, productionCompany, distributor, director) {
+    this.title = title;
+    this.releaseYear = releaseYear;
+    this.productionCompany = productionCompany;
+    this.distributor = distributor;
+    this.director = director;
+  }
+}
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!
+NOTE: write code that will filter data to eliminate commas in production company name and/or and empty string value.
+!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
+// Use fs.readFileSync() to store filtered dataset in a new variable:
+let filteredDataString = fs.readFileSync("2018entries.json", "utf8");
+
+// Convert the above variable to a JavaScript object for future manipulation:
+filteredDataObject = JSON.parse(filteredDataString);
+
+// Make an array to store objects created from strings found in "data" portion of dataset:
+const dataObjectsArray = [];
+
+// Create a function that will build new objects from the FilmLocations class:
+function stringToObject(string) {
+  // Split up the entry string into its individual parts.
+  let entryArray = string.split(",");
+
+  // Create a new object and provide the proper values.
+  let filmObject = new FilmLocations(
+    entryArray[0],
+    entryArray[1],
+    entryArray[4],
+    entryArray[5],
+    entryArray[6]
+  );
+
+  // Return new object to call location.
+  return filmObject;
+}
+
+// Loop through each string inside of the data key array inside the "filteredDataObject" variable.
+// Remember that: inside filteredDataObject there is a key called "data" holding an array of strings.
+for (let i = 0; i < filteredDataObject.data.length; i++) {
+  // Grab the current string inside the data key inside the filteredDataObject, send it to the stringToObject function to convert it to an object using the FilmLocations class, and then whatever it returns to us, push it into the dataObjectsArray.
+  dataObjectsArray.push(stringToObject(filteredDataObject.data[i]));
+}
+
+console.log(dataObjectsArray);
