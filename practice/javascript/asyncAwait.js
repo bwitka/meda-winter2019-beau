86ee24577176d51fa18a2a3d6718cf6d6cbc9596
Beau Witka <bwitka@gmail.com>
2019-12-04 13:05:36 -0800

    Add asyns/await practice file to JavaScript practice directory

---
+++ practice/javascript/asyncAwait.js
@@ -0,0 +1,42 @@
+/***** 
+ HOMEWORK(12/4/19): 
+
+ *****/
+
+// async function firstAsync() {
+//   return 27;
+// }
+
+// console.log(firstAsync());
+
+// firstAsync().then(results => {
+//   console.log(results);
+// });
+
+let updateThis = 0;
+
+async function firstAsync() {
+  let action = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value to send to the Promise so it completes.");
+    }, 3000);
+  });
+
+  console.log(updateThis);
+  updateThis = 1;
+  console.log(updateThis);
+  let result = await action;
+
+  updateThis = result;
+  console.log(updateThis);
+
+  console.log(result);
+
+  return updateThis;
+}
+
+let something = firstAsync();
+
+console.log(something);
+
+console.log("I will run after the first async function.");
