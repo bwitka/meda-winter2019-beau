adfd923b629f71e8566590bbc743fc76edb36e41
Beau Witka <bwitka@gmail.com>
2019-12-09 12:50:41 -0800

    Practice with JSX and React.createElement in index.js

--- practice/react-practice/my-first-app/src/index.js
+++ practice/react-practice/my-first-app/src/index.js
@@ -9,9 +9,31 @@ type,
 )
 */
 
-const title = React.createElement("h1", null, "Hello World!");
+const title = React.createElement("h1", { id: "top" }, "Hello World!");
+
+const someTitle = "Welcome to my React website!";
+
+const footer = (
+  <div id="footer">
+    <p>{someTitle}</p>
+    <ul>
+      <li>List Item 1</li>
+    </ul>
+  </div>
+);
+
+const footer2 = React.createElement("div", { id: "footer" });
 
 const greeting = React.createElement("p", null, "Hello there!");
+
+const idOfDiv = "test";
+
+const app = (
+  <div id={idOfDiv}>
+    {title}
+    {greeting}
+  </div>
+);
 
 // ReactDOM.render(element, container[, callback])
 ReactDOM.render(title, document.getElementById("root"));
