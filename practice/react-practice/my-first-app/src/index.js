ba0628a80e2642802c41c2176f7b9b42ea328906
Beau Witka <bwitka@gmail.com>
2019-12-10 11:27:17 -0800

    Create simple function and class components in index.js

--- practice/react-practice/my-first-app/src/index.js
+++ practice/react-practice/my-first-app/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Create our first component, barebones.
+// first argument is name of element, second argument is attributes, third is content of element.
+const title = React.createElement(
+  "h1",
+  {
+    id: "top"
+  },
+  "Hello World!"
+);
+
+const paragraph = React.createElement("p", null, "This is a paragraph!");
+
 let myFirstReactElement = <h1 id="firstHeader">Hello, World!</h1>;
 
 // SYNTAX: ReactDOM.render(element, container[, callback])
@@ -9,7 +21,60 @@ ReactDOM.render(myFirstReactElement, document.getElementById("root"));
 /****** INTRODUCING JSX! ******/
 // Note on using JSX: to run any JS variable, place the variable name between curly braces {<js_variable_name>}. Also, anything written in JSX should go between parentheses.
 
-let text = <p>{someJSVariable}</p>;
+// let text = <p>{someJSVariable}</p>;
+
+/****** FUNCTION COMPONENTS ******/
+
+// Note: whenever you have a function, you need to return JSX.
+function About() {
+  return <p>This is officially a React function component.</p>;
+}
+
+// Important note!!!: always be sure to capitalize your function names (i.e. don't start with a lowercase letter).
+
+// Note: 'props' refers to React properties.
+function H1Generator(props) {
+  return <h1>{props.text}</h1>;
+}
+
+const H2Generator = props => {
+  return <h2>{props.something}</h2>;
+};
+
+/****** CLASS COMPONENTS ******/
+
+class Counter extends React.Component {
+  constructor() {
+    // note: super() runs the constructor of the class it extends.
+    super();
+
+    this.state = {
+      score: 0
+    };
+  }
+
+  render() {
+    return <p>The current score is: </p>;
+  }
+}
+
+const idOfDiv = "test";
+
+// JSX version of createElement()
+const app = (
+  <div id={idOfDiv}>
+    <About tempScore={3} />
+    <H1Generator text="Hi, this is custom text" />
+    <H1Generator text="This is some other line!" />
+    <H2Generator something="Some text" />
+    <Counter />
+    {title}
+    {paragraph}
+  </div>
+);
+
+// Render component to target element.
+ReactDOM.render(app, document.getElementById("root"));
 
 /* SYNTAX:
 React.createElement(
